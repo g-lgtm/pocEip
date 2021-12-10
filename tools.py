fcfb3091b1950c35dabf26ecacece491e2ca5368
gael <gael.leroy666@gmail.com>
2021-12-10 21:01:47 +0100

    first commit

---
+++ tools.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+TARGET_COLORS = {"Red": (255, 0, 0), "Yellow": (255, 255, 0), "Green": (0, 255, 0), "White": (255, 255, 255), "Black": (0, 0, 0)}
+
+def color_difference(color1, color2):
+    return sum([abs(component1-component2) for component1, component2 in zip(color1, color2)])
+
+def getColor(rgbColor : tuple):
+    my_color = rgbColor
+    differences = [[color_difference(my_color, target_value), target_name] for target_name, target_value in TARGET_COLORS.items()]
+    differences.sort()
+    return (differences[0][1])
---
+++ wholeLeaf.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+from os import sep
+from PIL import Image
+from tools import getColor
+
+class branchItem:
+    def __init__(self, path : str):
+        self.path = path
+        self.image = Image.open(path)
+        self.pixels = self.image.load()
+    
+    def getStats(self):
+        x, y = 0, 0
+        maxX, maxY = self.image.size
+        lock, inLock = False, False
+        block = [()]
+        blocks = [[()]]
+        nbr = 0
+        block.clear()
+        blocks.clear()
+        for y in range(maxY):
+            for x in range(maxX):
+                if getColor(self.pixels[x, y]) == "Green" or getColor(self.pixels[x, y]) == "Yellow":
+                    inLock = True
+                    block.append(self.pixels[x, y])
+            if inLock:
+                lock = True
+            elif lock:
+                lock = False
+                blocks.append(block.copy())
+                block.clear()
+                nbr += 1
+            inLock = False
+        length = 0
+        result = [(1, 1, 1)]
+        result.clear()
+        for id in range(len(blocks)):
+            length = len(blocks[id]) + 2
+            if length % 2 != 0:
+                length += 1
+            GCount, YCount = 0, 0
+            for blk in blocks[id]:
+                if getColor(blk) == "Green":
+                    GCount += 1
+                elif getColor(blk) == "Yellow":
+                    YCount += 1
+            totalSize = GCount + YCount
+            if totalSize != 0:
+                gInt = int(GCount * 100 / totalSize * 100)
+                yInt = int(YCount * 100 / totalSize * 100)
+                if gInt + yInt < 10000:
+                    yInt = yInt + (10000 - (gInt + yInt))
+                result.append((id + 1, gInt / 100, yInt / 100))
+        return result
+
+champ = branchItem("full.png")
+status = champ.getStats()
+for id, green, yellow in status:
+    print("id:", id, "Green:", green, "% yellow", yellow, "%", sep=' ')
