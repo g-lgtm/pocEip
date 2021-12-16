745c23f1caa1563caefc4c51a919b0d9ea3cbb21
Martin Saison <martinsaison@MacBook-Pro-de-Martin.local>
2021-12-16 23:10:03 +0100

    pushing camera screen with pitcure taken

--- _app/lib/camera.dart
+++ _app/lib/camera.dart
@@ -1,49 +1,91 @@
-//
-//
-//
-//
-//
-
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 
-late List<CameraDescription> cameras;
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
 
-class CameraApp extends StatefulWidget {
+  final CameraDescription camera;
+
   @override
-  _CameraAppState createState() => _CameraAppState();
+  TakePictureScreenState createState() => TakePictureScreenState();
 }
 
-class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
-    controller = CameraController(cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.high,
+    );
+    _initializeControllerFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CameraPreview(controller),
+    return Scaffold(
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(Icons.camera_alt, color: Colors.green.shade100),
+      ),
     );
   }
 }
 
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Display the Picture')
+      ),
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
